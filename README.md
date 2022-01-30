1c7c2854de5d6bffc70768dcb366c9fa503e1f23
manu.antropo <manu.antropo@hotmail.com>
2022-01-30 16:27:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProyectoDemoCursoCorreto
+# ProyectoDemoCursoCorreto
---
+++ personalProjects/test/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita el texto pana  <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Da click aquí comparre. No le raje
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
